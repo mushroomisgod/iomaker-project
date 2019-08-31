a4b6ab052aaee11be7752329b62b57c2b714dd7b
Tomoyoshi Yamamoto <tomoyoshi.usa@gmail.com>
2019-08-31 17:03:13 +0900

    moved object to 一般化

--- js/editor.engine.js
+++ js/editor.engine.js
@@ -24,35 +24,7 @@ function updateSimpleMes() {
 //users io game data
 var gProjects = {
     name : [],
-    code : [
-        {
-            moveWhen : "buttonPress",
-            moveKeyBindEnabled : true,
-            moveKeyBind: [],
-            changeDirWhen : "buttonPress",
-            directionKeyBindEnabled : false,
-            directionKeyBind : [{ keyCodeB: 13, directionRL: "right", moveRL: false, directionUD: "up", moveUD: false }],
-            playerAppearPointSettings : "size",
-            playerLibrarySprite : "./assets/samplePack/car.png",
-            playerUploadSprite : "none",
-            usePlayerSprite : "library",
-            playerAttackSetting : { type:"shoot", keyCode:13 },
-            playerAttackLibrarySprite : "./assets/samplePack/axe.png",
-            playerAttackuploadSprite : "none",
-            usePlayerAttackSprite : "library",
-            howCollectablesSpawn : "randomPoint",
-            howMuchCollectablesSpawn : "alot",
-            howManyCollectablePoint : 0,
-            collectablesLibrarySprite : "./assets/samplePack/coin.png",
-            collectablesUploadSprite : "none",
-            collectablesSpriteType : "library",
-            floorLibrarySprite : "./assets/samplePack/hexagon.png",
-            floorUploadSprite : "none",
-            useFloorTexture : "library",
-            useGameTitle : "text",
-            uploadedGameTitleImage : "none"
-        }
-    ]
+    code : []
 };
 
 function selectAssetSimple(ele) {
--- js/editor.io.js
+++ js/editor.io.js
@@ -9,6 +9,7 @@ var updateData = function () {
         } else if (dataInput.type == "makeNewGame" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.name.push( dataInput.gname );
+                gProjects.code.push( { moveWhen : "buttonPress", moveKeyBindEnabled : true, moveKeyBind: [], changeDirWhen : "buttonPress", directionKeyBindEnabled : false, directionKeyBind : [{ keyCodeB: 13, directionRL: "right", moveRL: false, directionUD: "up", moveUD: false }], playerAppearPointSettings : "size", playerLibrarySprite : "./assets/samplePack/car.png", playerUploadSprite : "none", usePlayerSprite : "library", playerAttackSetting : { type:"shoot", keyCode:13 }, playerAttackLibrarySprite : "./assets/samplePack/axe.png", playerAttackuploadSprite : "none", usePlayerAttackSprite : "library", howCollectablesSpawn : "randomPoint", howMuchCollectablesSpawn : "alot", howManyCollectablePoint : 0, collectablesLibrarySprite : "./assets/samplePack/coin.png", collectablesUploadSprite : "none", collectablesSpriteType : "library", floorLibrarySprite : "./assets/samplePack/hexagon.png", floorUploadSprite : "none", useFloorTexture : "library", useGameTitle : "text", uploadedGameTitleImage : "none" } );
                 updateGameLaunchers();
             }
         } else if( dataInput.type == "newMoveKeyBind" ) {
