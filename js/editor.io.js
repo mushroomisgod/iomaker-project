151aa9e75d8f58f4e64b17312ff227bf68c36fd8
Tomoyoshi Yamamoto <tomoyoshi.usa@gmail.com>
2019-09-01 22:43:19 +0900

    more padding fix, and some saving tech

--- js/editor.io.js
+++ js/editor.io.js
@@ -16,26 +16,31 @@ var updateData = function () {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.oprojectID].moveKeyBind.push({ whichKey: "Enter", keyCodeA: 13, toX : 0, toY : 0});
                 updateCodeBlocks(dataInput.oprojectID);
+                saveProjectShou()
             }
         } else if( dataInput.type == "newChangeDirKeyBind" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.oprojectID].directionKeyBind.push({ keyCodeB: 13, directionRL: "right", moveRL: false, directionUD: "up", moveUD: false });
                 updateCodeBlocks(dataInput.oprojectID);
+                saveProjectShou()
             }
         } else if( dataInput.type == "deletePlayerMovementCodeBlock" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].moveKeyBind.splice( dataInput.whichI, 1 );
                 updateCodeBlocks(dataInput.pID);
+                saveProjectShou()
             }
         } else if( dataInput.type == "deletePlayerDirectionCodeBlock" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].directionKeyBind.splice( dataInput.whichI, 1 );
                 updateCodeBlocks(dataInput.pID);
+                saveProjectShou()
             }
         } else if( dataInput.type == "updatePlayerMovementKeyBindOption" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].moveKeyBind[dataInput.whichI].keyCodeA = parseInt(dataInput.keyCodeAA);
                 updateCodeBlocks(dataInput.pID);
+                saveProjectShou()
             }
         } else if( dataInput.type == "updateRLdirectionPlayer" ) {
             if( dataInput.user == loggedUser ) {
@@ -45,6 +50,7 @@ var updateData = function () {
                     gProjects.code[dataInput.pID].moveKeyBind[dataInput.whichI].toX = -1 * Math.abs(parseInt(gProjects.code[dataInput.pID].moveKeyBind[dataInput.whichI].toX));
                 }
                 updateCodeBlocks(dataInput.pID);
+                saveProjectShou()
             }
         } else if( dataInput.type == "updateUDdirectionPlayer" ) {
             if( dataInput.user == loggedUser ) {
@@ -54,6 +60,7 @@ var updateData = function () {
                     gProjects.code[dataInput.pID].moveKeyBind[dataInput.whichI].toY = -1 * Math.abs(parseInt(gProjects.code[dataInput.pID].moveKeyBind[dataInput.whichI].toY));
                 }
                 updateCodeBlocks(dataInput.pID);
+                saveProjectShou()
             }
         } else if( dataInput.type == "changeRLspeedPlayer" ) {
             if( dataInput.user == loggedUser ) {
@@ -73,11 +80,13 @@ var updateData = function () {
                     gProjects.code[dataInput.pID].moveWhen = "buttonPress";
                 }
                 changeWhatToMoveCondition();
+                saveProjectShou()
             }
         } else if( dataInput.type == "updateKeyForDirectionKeyBindBlock" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].directionKeyBind[dataInput.whichI].keyCodeB = parseInt(dataInput.value);
                 updateCodeBlocks(dataInput.pID);
+                saveProjectShou()
             }
         } else if( dataInput.type == "updateRLdirectionPlayerChoose" ) {
             if( dataInput.user == loggedUser ) {
@@ -103,6 +112,7 @@ var updateData = function () {
                     gProjects.code[dataInput.pID].directionKeyBind[dataInput.whichI].moveRL = true;
                 }
                 updateCodeBlocks(dataInput.pID);
+                saveProjectShou()
             }
         } else if( dataInput.type == "UDmoveOrNotUpdate" ) {
             if( dataInput.user == loggedUser ) {
@@ -111,6 +121,7 @@ var updateData = function () {
                 } else if( dataInput.value == "yesMove" ) {
                     gProjects.code[dataInput.pID].directionKeyBind[dataInput.whichI].moveUD = true;
                 }
+                saveProjectShou()
                 updateCodeBlocks(dataInput.pID);
             }
         } else if( dataInput.type == "whatToCDirUpdate" ) {
@@ -121,6 +132,7 @@ var updateData = function () {
                     gProjects.code[dataInput.pID].changeDirWhen = "buttonPress";
                 }
                 updateWhenToCDirOption();
+                saveProjectShou()
             }
         } else if( dataInput.type == "changePointAppearSettingPlayer" ) {
             if( dataInput.user == loggedUser ) {
@@ -131,8 +143,8 @@ var updateData = function () {
                 } else if( dataInput.value == "nothing" ) {
                     gProjects.code[dataInput.pID].playerAppearPointSettings = "nothing";
                 }
-                document.getElementById('iframeid').src += '';
                 updatePlayerPointAppearance();
+                saveProjectShou()
             }
         } else if( dataInput.type == "chooseSpriteForPlayerAttack" ) {
             if( dataInput.user == loggedUser ) {
@@ -145,8 +157,8 @@ var updateData = function () {
                 } else if( dataInput.value == "bullet" ) {
                     gProjects.code[dataInput.pID].playerAttackLibrarySprite = "./assets/samplePack/bullet.png";
                 }
-                document.getElementById('iframeid').src += '';
                 updatePlayerAttackLibrarySelectedSprite();
+                saveProjectShou()
             }
         } else if( dataInput.type == "chooseSpriteForPlayer" ) {
             if( dataInput.user == loggedUser ) {
@@ -159,8 +171,8 @@ var updateData = function () {
                 } else if( dataInput.value == "person" ) {
                     gProjects.code[dataInput.pID].playerLibrarySprite = "./assets/samplePack/person.png";
                 }
-                document.getElementById('iframeid').src += '';
                 updatePlayerLibrarySelectedSprite();
+                saveProjectShou()
             }
         } else if( dataInput.type == "setWhichSourceToUsePlayerAttack" ) {
             if( dataInput.user == loggedUser ) {
@@ -169,57 +181,62 @@ var updateData = function () {
                 } else if( dataInput.value == "libraryImage" ) {
                     gProjects.code[dataInput.pID].usePlayerAttackSprite = "libraryImage";
                 }
-                document.getElementById('iframeid').src += '';
                 whichSpriteTypeToUseForPlayerAttackUpdate();
+                saveProjectShou()
             }
         } else if( dataInput.type == "changePlayerAttackKeyBind" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].playerAttackSetting.keyCode = parseInt(dataInput.keyCode);
             }
-            document.getElementById('iframeid').src += '';
             updatePlayerAttackTriggerKeyCode();
+            saveProjectShou()
         } else if( dataInput.type == "changePlayerAttackType" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].playerAttackSetting.type = dataInput.value;
-                document.getElementById('iframeid').src += '';
                 updatePlayerAttackTypeSelector();
+                saveProjectShou()
             }
         } else if( dataInput.type == "uploadPlayerWeaponImage" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].playerAttackuploadSprite = dataInput.value;
-                document.getElementById('iframeid').src += '';
                 updateUploadedDataScheme();
+                saveProjectShou()
             }
         } else if( dataInput.type == "updateWhichPlayerSprite" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].usePlayerSprite = dataInput.value;
-                document.getElementById('iframeid').src += '';
                 updateWhichPlayerSpriteType();
+                saveProjectShou()
             }
         } else if( dataInput.type == "uploadPlayerSprite" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].playerUploadSprite = dataInput.value;
                 updateUploadedDataScheme();
+                saveProjectShou()
             }
         } else if( dataInput.type == "uploadCollectablesSprite" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].collectablesUploadSprite = dataInput.value;
                 updateUploadedDataScheme();
+                saveProjectShou()
             }
         } else if( dataInput.type == "collectableSpawnOptionUpdate" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].howCollectablesSpawn = dataInput.value;
                 updateHowSpawnSelector();
+                saveProjectShou()
             }
         } else if( dataInput.type == "changeHowMuchCollectablesSpawn" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].howMuchCollectablesSpawn = dataInput.value;
                 updateHowMuchSpawnSelector();
+                saveProjectShou()
             }
         } else if( dataInput.type == "changeHowManyPointsPerInput" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].howManyCollectablePoint = parseInt(dataInput.value);
                 updateHowManyPointsPerCollectSelector();
+                saveProjectShou()
             }
         } else if( dataInput.type == "chooseLibrarySpriteForCollectables" ) {
             if( dataInput.user == loggedUser ) {
@@ -233,11 +250,13 @@ var updateData = function () {
                     gProjects.code[dataInput.pID].collectablesLibrarySprite = "./assets/samplePack/peach.png";
                 }
                 updateLibraryCollectableSpriteSelector();
+                saveProjectShou()
             }
         } else if( dataInput.type == "whichUseCollectable" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].collectablesSpriteType = dataInput.value;
                 updateCollectablesSpriteTypeSelector();
+                saveProjectShou()
             }
         } else if( dataInput.type == "whichLibraryFloorTexture" ) {
             if( dataInput.user == loggedUser ) {
@@ -246,26 +265,31 @@ var updateData = function () {
                 } else {
                     gProjects.code[dataInput.pID].floorLibrarySprite = "./assets/samplePack/grass.png";
                 }
+                saveProjectShou()
                 updateWhichLibraryTextureSelected();
             }
         } else if( dataInput.type == "uploadFloorTexture" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].floorUploadSprite = dataInput.value;
             }
+            saveProjectShou()
             updateUploadedFloorTexturePreview();
         } else if( dataInput.type == "whichTouseForFloorTextureUpdate" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].useFloorTexture = dataInput.value;
+                saveProjectShou()
                 updateWhichFloorTextureTypeUseSelector();
             }
         } else if( dataInput.type == "updateWhichTypeOfTitleImageToUseSelector" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].useGameTitle = dataInput.value;
+                saveProjectShou()
                 updateWhichTitleTypeToUseSelector();
             }
         } else if( dataInput.type == "uploadGameTitleImage" ) {
             if( dataInput.user == loggedUser ) {
                 gProjects.code[dataInput.pID].uploadedGameTitleImage = dataInput.value;
+                saveProjectShou()
                 updateUploadedGameTitleImagePreview();
             }
         }
